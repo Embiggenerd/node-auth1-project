b2aa1e6335c158f41c22cc3e62d33d7e7e277f24
igor <embiggenerd@gmail.com>
2020-02-22 14:27:02 -0500

    server up listening

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const server = express()
+
+server.use(express.json())
+
+server.use((err, req, res, next) => {
+    console.log(err)
+    res.status(err.httpStatusCode || 500).json({
+        message:err.message
+    })
+})
+
+module.exports = server
