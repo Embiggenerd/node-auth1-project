420733ae4eb243c7984730faf30488d14d19ae1a
igor <embiggenerd@gmail.com>
2020-02-22 14:45:12 -0500

    router initialized

---
+++ router/api.js
@@ -0,0 +1,29 @@
+const { Router } = require('express')
+
+router = Router()
+
+router.post('/register', async (req, res, next) => {
+    try {
+        res.json('register')
+    } catch (e) {
+        next(e)
+    }
+})
+
+router.post('/login', (req, res, next) => {
+    try {
+        res.json('/login')
+    } catch (e) {
+        next(e)
+    }
+})
+
+router.get('/users', (req, res, next) => {
+    try {
+        res.json('/users')
+    } catch (e) {
+        next(e)
+    }
+})
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require('express')
 const server = express()
 
+const { apiRouter } = require('./router')
+
 server.use(express.json())
+
+server.use('/api', apiRouter)
 
 server.use((err, req, res, next) => {
     console.log(err)
     res.status(err.httpStatusCode || 500).json({
-        message:err.message
+        message: err.message
     })
 })
 
